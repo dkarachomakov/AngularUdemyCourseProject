55cb8342186782ae3d8bb0e86ba0180501f1314d
Doboromir Karachomakov <dkarachomakov@gmail.com>
2019-12-18 20:08:29 +0200

    AngularFundamentals2

--- AngularFundamentals/fundamentals/src/app/courses.component.ts
+++ AngularFundamentals/fundamentals/src/app/courses.component.ts
@@ -9,7 +9,21 @@ import { CouresService } from './courses.service';
                     <li *ngFor="let course of courses">
                         {{ course }}
                     </li>
-                </ul>        
+                </ul>    
+                
+                <img src="{{ imageUrl }}" />
+                <img [src]="imageUrl" />
+
+                <table>
+                    <tr>
+                        <td [attr.colspan]="colspan"></td>
+                    </tr>
+                </table>
+                <div (click)="onDivSurroundingButtonClick($event)">
+                    <button (click)="onButtonClick($event)" class="btn btn-primary" [class.active]="isButtonActive" [style.backgroundColor]="isButtonActive ? 'blue' : 'white'">Save</button>
+                </div>
+                <input #email (keyup.enter)="onEnterKeyUp($event, email.value)"/>
+                
     `
 })
 
@@ -17,10 +31,23 @@ import { CouresService } from './courses.service';
 export class CoursesComponent{
     title = "List of courses"
     courses;
+    imageUrl="https://placekitten.com/420/320"
+    colspan=2;
+    isButtonActive=true;
 
     constructor(service: CouresService) {
         this.courses = service.getCourses();
     }
 
-    
+    onButtonClick($event){
+        //$event.stopPropagation();
+        console.log("The button was clicked", $event);
+    }
+    onDivSurroundingButtonClick($event){
+        console.log("Div was clicked", $event);
+    }
+    onEnterKeyUp($event, email){
+        console.log($event.target.value);
+        console.log(email);
+    }
 }
